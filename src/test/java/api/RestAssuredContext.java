eab6a3a76b45edbfb453f648f0a61ec20eb2e06e
Yurii Kononenko <yurii_kononenko@epam.com>
2019-03-25 10:23:02 +0200

    api tests example

---
+++ src/test/java/api/RestAssuredContext.java
@@ -0,0 +1,13 @@
+package api;
+
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+
+public class RestAssuredContext {
+
+    public static Response response;
+    public static ValidatableResponse validatableResponse;
+    public static RequestSpecification request;
+
+}
